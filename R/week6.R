79cad95611fccfdeb107b2a50ea1b7851bcd90c1
Ben Elliott <ellio747@umn.edu>
2026-02-24 15:43:23 -0600

    part 2 - complete

--- R/week6.R
+++ R/week6.R
@@ -1,9 +1,14 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
-# library()
+library(tidyverse)
 # library()
 # library()
 
 # Data Import
 citations <- stri_read_lines("../data/cites.txt")
+citations_txt <- str_subset(citations, pattern = "\\S")
+cat("The number of blank lines eliminated was", length(citations) - length(citations_txt))
+str_c("The average number of characters/citation was", mean(str_count(citations)))
+
+# Data Cleaning
